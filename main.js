6fba7a22d743d83e3be3169fafd19ea37fdeae98
Mateusz Paczkowski <mattypaczkowski@gmail.com>
2021-01-05 16:48:37 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+const name = "Mateusz";
+const developer = "programistą";
+
+console.log("Cześć!");
+console.log(`Jestem ${name} i moim celem jest zostać ${developer}.`);
+console.log("Trzymaj za mnie kciuki!");
