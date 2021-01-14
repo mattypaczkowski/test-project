eb9c895b29db20fa996b49de13f117c1cae8dab8
Mateusz Paczkowski <mattypaczkowski@gmail.com>
2021-01-14 16:47:54 +0000

    list html and JavaScript functions

--- main.js
+++ main.js
@@ -1,46 +1,47 @@
-// const name = "Mateusz";
-// const developer = "programistą";
+/* First lines in JavaScript */
 
-// console.log("Cześć!");
-// console.log(`Jestem ${name} i moim celem jest zostać ${developer}.`);
-// console.log("Trzymaj za mnie kciuki!");
+const name = "Mateusz";
+const developer = "programistą";
 
-// const articel = document.querySelector('.article--js');
+console.log("Cześć!");
+console.log(`Jestem ${name} i moim celem jest zostać ${developer}.`);
+console.log("Trzymaj za mnie kciuki!");
 
-// articel.innerHTML = `<h4 class="article__tittle">Czym są frameworki i biblioteki</h4>
-// <p class="article__paragraph">
-// Zacznijmy od tego, co to framework lub biblioteka. Internet jest pełen definicji i niektóre z nich zakładają, że zastosowanie zasady odwrócenia sterowania jest główną różnicą między tymi dwoma pojęciami. Inne zaś twierdzą, że biblioteka jest zbiorem metod specyficznych dla zadania, a framework to zbyt szeroki termin, który mógłby oznaczać tyle, co biblioteka, zbiór bibliotek lub coś jeszcze bardziej zawiłego.
-// Na potrzeby tego artykułu wszyscy musimy mieć na uwadze, że zarówno biblioteka, jak i framework, są zbiorami metod mających na celu przyspieszenie rozwoju i wyodrębnienie typowych lub złożonych zadań.
-// </br>
-// <i>Artykuł powyżej znajdziesz w DOM. Powstał dzięki właściwością document.querySelector i artical.innerHTML.</i>
-// </p>`;
+/* How to use querySelector and inner HTML */
 
-// console.log(articel);
+const articel = document.querySelector(".article--js");
 
+articel.innerHTML = `<h4 class="article__tittle">Czym są frameworki i biblioteki</h4>
+ <p class="article__paragraph">
+ Zacznijmy od tego, co to framework lub biblioteka. Internet jest pełen definicji i niektóre z nich zakładają, że zastosowanie zasady odwrócenia sterowania jest główną różnicą między tymi dwoma pojęciami. Inne zaś twierdzą, że biblioteka jest zbiorem metod specyficznych dla zadania, a framework to zbyt szeroki termin, który mógłby oznaczać tyle, co biblioteka, zbiór bibliotek lub coś jeszcze bardziej zawiłego.
+ Na potrzeby tego artykułu wszyscy musimy mieć na uwadze, że zarówno biblioteka, jak i framework, są zbiorami metod mających na celu przyspieszenie rozwoju i wyodrębnienie typowych lub złożonych zadań.
+ </br>
+ <i>Artykuł powyżej znajdziesz w DOM. Powstał dzięki właściwością document.querySelector i artical.innerHTML.</i>
+</p>`;
 
+/* How to use functions */
 
-/*const button = document.querySelector('.submit');
+function lineText(course, emoji) {
+  console.log(
+    `Właśnie jestem w połowie ${course} i sprawdzam jak działają funkcję ${emoji}`
+  );
+}
 
-button.addEventListener('click', function (event) {
-    event.preventDefault();
-    const x = document.querySelector('#input-x').value;
-    const y = document.querySelector('#input-y').value;
-    const nubmerOne = Number(x);
-    const numberTwo = Number(y);
-    const sum = nubmerOne + numberTwo;
-    
-    console.log(sum);
-    
-}); */
+lineText("kursu WTF", "🤔");
+
+const arrowFunction = (arrow, emoji) => {
+  console.log(`To samo tylko ${arrow} function ${emoji}`);
+};
+
+arrowFunction("arrow", "😎");
 
 function calculate(e) {
-    const x = document.querySelector('#input-x').value;
-    const y = document.querySelector('#input-y').value;
-    const nubmerOne = Number(x);
-    const numberTwo = Number(y);
-    const sum = nubmerOne + numberTwo;
-    
-    console.log(sum);
-    return false;
-    
-};
+  const x = document.querySelector("#input-x").value;
+  const y = document.querySelector("#input-y").value;
+  const nubmerOne = Number(x);
+  const numberTwo = Number(y);
+  const sum = nubmerOne + numberTwo;
+
+  console.log(sum);
+  return false;
+}
